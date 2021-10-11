dd47a3b8c457cbb927f31404c29b31ba384a3621
arnabdey03 <arnabdey021998@gmail.com>
2021-10-12 00:54:45 +0530

    minor updation and saving all scripts

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ app.use(cors())
 
 app.use("/posts", postRoutes)
 
+app.get("/", (req, res) => {
+	res.send("Hello, Welcome To Memories API")
+})
+
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-		.then(() => {
-			app.listen(PORT, () => {
-				console.log(`Server Running On Port ${PORT}`)
-			})
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server Running On Port ${PORT}`)
 		})
-		.catch((error) => {
-			console.log(error.message)
-		})
+	})
+	.catch((error) => {
+		console.log(error.message)
+	})
